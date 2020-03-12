9e056144e45d21d4c2cccf2e6fed33724e08a121
zameerhossain <zameer.hossain@gmail.com>
2020-03-13 05:32:00 +0600

    solve largest among three numbers problem

---
+++ largest_among_three_numbers.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b and a>c:
+    print("a is the largerst")
+elif  b>a and b>c:
+    print("b is the largerst")
+elif  c>a and c>b:
+    print("c is the largerst")
+
